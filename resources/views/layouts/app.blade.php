dc29a081c3864cd5f46f605e674dacfd32c5d982
jr1213 <mohammed.jr.hammad@gmail.com>
2024-05-26 21:26:03 +0300

    finish layouting

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livewire Chat App</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @livewireStyles
+</head>
+<body class="dark-theme">
+    <div class="container">
+    </div>
+    @livewireScripts
+</body>
+</html>
